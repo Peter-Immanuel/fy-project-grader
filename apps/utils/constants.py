2d5f5f094b0e45c67490f4cd555bd5e0404e0929
Peter-Immanuel <peterbemshima@gmail.com>
2023-06-28 16:46:18 +0100

    Feat: created grader schema

---
+++ apps/utils/constants.py
@@ -0,0 +1,17 @@
+
+
+STAFF_TITLE = (
+    ("Prof","Prof"),
+    ("Prof","Prof"),
+    ("Prof","Prof"),
+    ("Prof","Prof"),
+    ("Prof","Prof"),
+)
+
+
+STAFF_TYPE = (
+    ("Supervisor", "Supervisor"),
+    ("Internal_Evaluator", "Internal_Evaluator"),
+    ("External_Evaluator", "External_Evaluator"),
+    ("Supervisor_and_Evaluator", "Supervisor_and_evaluator"),
+)
