dfbb664460858a4a16f073c3b369bc311caac297
Peter-Immanuel <peterbemshima@gmail.com>
2023-07-01 16:38:20 +0100

    Feat: implemented dialog box and updated student details view

--- apps/grader/views.py
+++ apps/grader/views.py
@@ -14,6 +14,7 @@ class StudentDetailsCreationView(View):
     form = StudentDetailsForm
     # template = "students/student_form.html"
     template="components/students/student_form.html"
+    success_template="components/success-dialog.html"
     
     def get(self, request, *args, **kwargs):
         form = self.form()
@@ -23,7 +24,10 @@ class StudentDetailsCreationView(View):
         form = self.form(data=request.POST)
         if form.is_valid():
             form.create_record()
-            return HttpResponse("Form successfully submitted")
+            context = {
+                "message": "Thank you for updating your record"
+            }
+            return render(request, self.success_template, context)
         else:
             # import pdb; pdb.set_trace()
             return render(request, self.template, {"form":form})
